2ff99f802cfc4f6eb9bcf5b4b97c7a03047648d1
adityashirodkar1 <106297781+adityashirodkar1@users.noreply.github.com>
2023-08-10 18:32:38 +0530

    Update database.py
    remove mongodb url

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-connection_string = "mongodb+srv://adishiro:gintoki@cluster0.wlmssnp.mongodb.net/?retryWrites=true&w=majority"
+
 client = MongoClient(connection_string)
 db = client.get_database("ChatraMitra")
 users = db.get_collection("users")
